1bc58ee19532928860961ffa418ca118af717554
Archana Agrawal <105752655+archa-agrawal@users.noreply.github.com>
2022-11-16 14:43:17 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ knex.migrate.latest().then(() => {
       resave: true,
       saveUninitialized: false,
       cookie: {
-        secure: false,
+        secure: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: false,
         sameSite: false,
