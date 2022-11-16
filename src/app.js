5803fad93e71b1bad15fb83305db97b3c49c3625
Archana Agrawal <105752655+archa-agrawal@users.noreply.github.com>
2022-11-16 16:05:56 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ knex.migrate.latest().then(() => {
       credentials: true,
     })
   );
-  app.set('trust proxy', 1);
+  app.enable('trust proxy');
   app.use(
     session({
       secret: process.env.SECRET_KEY,
